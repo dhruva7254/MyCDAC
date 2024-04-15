cf245dd62fb5c6de597613f6d7b04aeca47c7bd4
dhruva7254 <87358476+dhruva7254@users.noreply.github.com>
2024-04-16 01:17:42 +0530

    Add files via upload

---
+++ Lab Practice/Dhruva/Python/Python _Practice/Python Programming/Day6/set_tuple_dict assignment/Tuple/q1.py
@@ -0,0 +1,8 @@
+"""1. Reverse the following tuple
+aTuple = (10, 20, 30, 40, 50,60)
+Expected output:
+(60,50, 40, 30, 20, 10)"""
+
+aTuple = (10, 20, 30, 40, 50, 60)
+reversed_tuple = aTuple[::-1]
+print("Reversed tuple:", reversed_tuple)
